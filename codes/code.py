735187f4a77eb2c7aa79549cb6d8d6ed222f4451
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-05-08 10:50:47 +0530

    Update code.py

--- codes/code.py
+++ codes/code.py
@@ -4,3 +4,10 @@ probability = {1:frequency[1]/6,2:frequency[2]/6,3:frequency[3]/6}
 print(f'P(2) = {probability[2]}')
 print(f'P(1 or 3) = {probability[1]+probability[3]}')
 print(f'P(not 3) = {1 - probability[3]}')
+x = [1,2,3]
+y = [probability[1],probability[1]+probability[2],probability[1]+probability[2]+probability[3]]
+plt.plot(x,y,label = 'CDF')
+z = [probability[1],probability[2],probability[3]]
+plt.plot(x,z,label = 'PMF')
+plt.legend()
+plt.show()
